407717f0076bf12126cd21285a03b8272c87ce2c
Touko <touko.ristiranta@gmail.com>
2025-12-19 19:07:48 +0200

    add 2 more enemies

--- slime-harvester/src/settings.py
+++ slime-harvester/src/settings.py
@@ -30,17 +30,19 @@ TOWER_CONFIG = {
 
 ENEMY_CONFIG = {
     "red_slime": {"health": 1, "enemy_speed": 2, "enemy_image": "enemy_red.png", "next_type": 0},
-    "blue_slime": {"health": 1, "enemy_speed": 2, "enemy_image": "enemy_blue.png", "next_type": "red_slime"}
+    "blue_slime": {"health": 1, "enemy_speed": 2, "enemy_image": "enemy_blue.png", "next_type": "red_slime"},
+    "green_slime": {"health": 1, "enemy_speed": 3, "enemy_image": "enemy_green.png", "next_type": "blue_slime"},
+    "pink_slime": {"health": 1, "enemy_speed": 5, "enemy_image": "enemy_pink.png", "next_type": "green_slime"}
 }
 
 WAVE_CONFIG = {
     1: {"enemy_count": 5, "spawn_interval": 60, "enemy_type": "red_slime"},
     2: {"enemy_count": 10, "spawn_interval": 50, "enemy_type": "blue_slime"},
     3: {"enemy_count": 15, "spawn_interval": 40, "enemy_type": "red_slime"},
-    4: {"enemy_count": 20, "spawn_interval": 30, "enemy_type": "red_slime"},
-    5: {"enemy_count": 25, "spawn_interval": 20, "enemy_type": "red_slime"},
+    4: {"enemy_count": 10, "spawn_interval": 30, "enemy_type": "green_slime"},
+    5: {"enemy_count": 10, "spawn_interval": 20, "enemy_type": "pink_slime"},
     6: {"enemy_count": 30, "spawn_interval": 10, "enemy_type": "red_slime"},
-    7: {"enemy_count": 35, "spawn_interval": 10, "enemy_type": "red_slime"},
+    7: {"enemy_count": 20, "spawn_interval": 10, "enemy_type": "pink_slime"},
     8: {"enemy_count": 40, "spawn_interval": 5, "enemy_type": "red_slime"},
     9: {"enemy_count": 100000000, "spawn_interval": 5, "enemy_type": "red_slime"}
 
